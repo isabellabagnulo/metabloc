0c8f01b0cfbd91906996bd107e31d370416e5c8e
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-02-10 11:24:05 +0100

    prova scroll

--- scripts.js
+++ scripts.js
@@ -10,3 +10,22 @@ document.addEventListener(
   backToTop(),
   swiper()
 )
+
+const myelement = document.querySelector("back-to-top")
+
+function SmoothVerticalScrolling(e, time, where) {
+  var eTop = e.getBoundingClientRect().top
+  var eAmt = eTop / 100
+  var curTime = 0
+  while (curTime <= time) {
+    window.setTimeout(SVS_B, curTime, eAmt, where)
+    curTime += time / 100
+  }
+}
+
+function SVS_B(eAmt, where) {
+  if (where == "center" || where == "") window.scrollBy(0, eAmt / 2)
+  if (where == "top") window.scrollBy(0, eAmt)
+}
+
+SmoothVerticalScrolling(myelement, 275, "center")
