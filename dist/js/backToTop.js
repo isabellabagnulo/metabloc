518ee38330f09b55c241043f274ce93ddad13641
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-02-10 10:48:35 +0100

    easing

--- dist/js/backToTop.js
+++ dist/js/backToTop.js
@@ -7,7 +7,38 @@ function backToTop() {
     } else {
       arrowTop.classList.remove("show")
     }
+    arrowTop.addEventListener("click", () => {
+      const html = document.documentElement
+      const body = document.body
+      return (html, body).animate(
+        {
+          top: 0,
+        },
+        {
+          duration: 500,
+          easing: "ease-in-out",
+        }
+      )
+    })
   })
 }
 
 export { backToTop }
+
+//   // Back to top button
+// ;(function () {
+//   $(document).ready(function () {
+//     return (
+//       $(window).scroll(function () {
+//         return $(window).scrollTop() > 600
+//           ? $("#back-to-top").addClass("show")
+//           : $("#back-to-top").removeClass("show")
+//       }),
+//       $("#back-to-top").click(function () {
+//         return $("html,body").animate({
+//           scrollTop: "0",
+//         })
+//       })
+//     )
+//   })
+// }.call(this))
