cb51bacfebcb9a671e11d0ec0a3aa26c29a0f376
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-03-02 15:45:30 +0100

    .

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
-import { backToTop } from "./dist/js/backToTop.js"
-import { verticalMenu } from "./dist/js/vertical-menu.js"
-import { swiper } from "./dist/js/swiper.js"
+document.addEventListener("DOMContentLoaded", backToTop())
 
-document.addEventListener(
-  "DOMContentLoaded",
-  verticalMenu(),
-  backToTop(),
-  swiper()
-)
+//BACK TO TOP
+function backToTop() {
+  const arrowTop = document.querySelector(".back-to-top")
+  window.addEventListener("scroll", () => {
+    const scrollPosition = window.scrollY
+    if (scrollPosition > 600) {
+      arrowTop.classList.add("show")
+    } else {
+      arrowTop.classList.remove("show")
+    }
+    arrowTop.addEventListener("click", () => {
+      const html = document.documentElement
+      const body = document.body
+      return (html, body).animate(
+        {
+          top: 0,
+        },
+        {
+          duration: 500,
+          easing: "ease-in-out",
+        }
+      )
+    })
+  })
+}
 
 // STICKY MENU
 const body = document.body
@@ -35,4 +52,41 @@ window.addEventListener("scroll", () => {
     body.classList.add(scrollUp)
   }
   lastScroll = currentScroll
+})
+
+// SWIPER
+import Swiper from "https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js"
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  calculateHeight: true,
+  pagination: {
+    clickable: true,
+    dynamicBullets: true,
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 10000,
+    pauseOnMouseEnter: true,
+    waitForTransition: true,
+  },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  on: {
+    init: function () {
+      console.log("swiper initialized")
+    },
+  },
+})
+
+const team = document.querySelector("#ourteam")
+team.addEventListener("mouseover", () => {
+  swiper.autoplay.start()
 })
